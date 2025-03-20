581d002f5f453de6be610f5494ca4b9f9dac4051
billy <billyouko9@gmail.com>
2025-03-20 17:23:31 +0300

    Modified open file in create test files

--- create_test_files.py
+++ create_test_files.py
@@ -1,4 +1,4 @@
-import os 
+import os
 
 folder_path = "C:\\Users\\j\\Desktop\\python_projects\\test_file"
 
@@ -10,6 +10,6 @@ if not os.path.exists(folder_path):
 filenames = ["image1.jpg", "document1.txt", "report.pdf", "photo.png"]
 
 for file in filenames:
-    open(os.path.join(folder_path, file), 'w').close()
-
+    with open(os.path.join(folder_path, file), 'w') as f:
+        pass  # This ensures the file is created and closed properly.
 print("Folder and files created successfully!")
